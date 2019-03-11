6dffabc22f9d4db1ff4afb8498cdd8e43e70b25b
Allan Nordhøy <epost@anotheragency.no>
2019-03-12 00:36:06 +0100

    Spelling: Could not register, ... → …

--- qml/scripts/PasswordCreationPageActions.js
+++ qml/scripts/PasswordCreationPageActions.js
@@ -27,9 +27,9 @@ function register () {
 
     var registerError = function (error) {
         if ( error.errcode === "M_USER_IN_USE" ) toast.show (i18n.tr("Username already taken"))
-        else if ( error.errcode === "M_INVALID_USERNAME" ) toast.show ( i18n.tr("The desired user ID is not a valid user name") )
+        else if ( error.errcode === "M_INVALID_USERNAME" ) toast.show ( i18n.tr("The desired user ID is not a valid username") )
         else if ( error.errcode === "M_EXCLUSIVE" ) toast.show ( i18n.tr("The desired user ID is in the exclusive namespace claimed by an application service") )
-        else toast.show ( i18n.tr("Registration on %1 failed...").arg((loginDomain || defaultDomain)) )
+        else toast.show ( i18n.tr("Could not register on %1…").arg((loginDomain || defaultDomain)) )
     }
 
     matrix.register ( desiredUsername.toLowerCase(), loginTextField.text, (loginDomain || defaultDomain), "UbuntuPhone", registerSuccess, registerError )
