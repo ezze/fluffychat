4777b14ffa7710e475519f32b4e040e7154cc69a
Allan Nordhøy <epost@anotheragency.no>
2019-03-12 00:16:02 +0100

    Spelling: Too many new

--- qml/scripts/ChatPageActions.js
+++ qml/scripts/ChatPageActions.js
@@ -254,7 +254,7 @@ function requestHistory ( event_id ) {
                 var indx = count - 1 - historyCount + eventFound
                 chatScrollView.positionViewAtIndex ( indx, ListView.Contain )
             }
-            else toast.show ( i18n.tr ( "Too much new messages" ) )
+            else toast.show ( i18n.tr ( "Too many new messages" ) )
         }
     }
 
