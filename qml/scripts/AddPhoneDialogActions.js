b8c3e6e5abe4086011fe5fe4c3c8533ebf2c1d14
Christian Pauly <christian-pauly@posteo.de>
2020-04-16 19:27:11 +0200

    Fix #160

--- qml/scripts/AddPhoneDialogActions.js
+++ qml/scripts/AddPhoneDialogActions.js
@@ -1,20 +1,20 @@
 // File: AddPhoneDialogActions.js
 // Description: Actions for AddPhoneDialog.qml
 
-function add ( address, dialogue ) {
-    
-    if ( address.charAt(0) === "0" ) address = address.replace( "0", matrix.countryTel )
-    client_secret = "SECRET:" + new Date().getTime()
+function add(address, dialogue) {
 
-    var callback = function ( response ) {
-        if ( response.error ) return toast.show ( response.error )
-        if ( response.sid ) {
+    if (address.charAt(0) === "0") address = address.replace("0", matrix.countryTel)
+    client_secret = "SECRET" + new Date().getTime()
+
+    var callback = function (response) {
+        if (response.error) return toast.show(response.error)
+        if (response.sid) {
             phoneSettingsPage.sid = response.sid
         }
     }
 
     // Verify this address with this matrix id
-    matrix.post ( "/client/r0/account/3pid/msisdn/requestToken", {
+    matrix.post("/client/r0/account/3pid/msisdn/requestToken", {
         client_secret: client_secret,
         country: matrix.countryCode,
         phone_number: address,
